e7baf1c3d8f474e1cc7160cd3f07728fab4dc99a
Gerardo Vidal <vidal_gerardo@ymail.com>
2023-01-15 16:18:42 -0600

    Created project code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var layouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var aboutRouter = require('./routes/about');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
